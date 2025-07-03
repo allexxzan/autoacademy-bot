4fd105866519a5dca826df5e1defed975cd81e3e
allexxzan <allexxzan@yandex.ru>
2025-07-03 20:53:27 +0300

    Update new_bot.py

--- new_bot.py
+++ new_bot.py
@@ -579,4 +579,8 @@ async def main():
 # Точка входа для запуска из консоли
 if __name__ == "__main__":
     import asyncio
-    asyncio.run(main())
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.create_task(main())
+    loop.run_forever()
